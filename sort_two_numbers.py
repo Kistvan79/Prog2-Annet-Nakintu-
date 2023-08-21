a4fb402a5c543dba4caac7118c2d6898724f87fc
Kistvan79 <annet.nakintu79@outlook.com>
2023-08-21 20:01:33 +0200

    uppdaterad sort_two_numbers

--- sort_two_numbers.py
+++ sort_two_numbers.py
@@ -1,11 +1,17 @@
-line = input("Input your integers --> ")
-a, b = line.split()
-a = int(a)
-b = int(b)
 
-if a > b:
-    print(b, a)
-else:
-    print(a, b)
+try:
+    line = input("Input your integers --> ")
+    a, b = line.split()
+    a = int(a)
+    b = int(b)
+    while 0 <= a <= 1000000 and 0 <= b <= 1000000:
+        if a > b:
+            print(b, a)
+            break
+        elif b < a:
+            print(a, b)
+            break
 
-#BOOOM slutfört!
+except ValueError:
+    print("Please input 2 integers from 0 to 1 000 000")
+   
