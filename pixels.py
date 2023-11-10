f458d7889fd3fb5f8db1258c30e9a9c4d397447b
Kistvan79 <annet.nakintu79@outlook.com>
2023-11-10 23:29:53 +0100

    ?

---
+++ pixels.py
@@ -0,0 +1,25 @@
+country_map = [
+    ".....###.",
+    "....#####",
+    "...######",
+    "..######.",
+    ".######..",
+    ".####....",
+    "####S#.#.",
+    "#####....",
+    ".##......",
+]
+# city = 0
+# pixels = 0
+# for i in country_map:
+#     print(i)
+#     for char in i:
+#         if char == "#":
+#             pixels += 1
+#         if char == "S":
+#             city += 1
+
+
+# print("Antal pixlar = ", pixels, "Antal city = ", city)
+
+    
